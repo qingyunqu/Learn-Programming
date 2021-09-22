76f5c80180049884dfe005c3ce5ae5968d35279e
liuyuanqiang <liuyuanqiang.yqliu@bytedance.com>
2021-09-23 00:07:52 +0800

    add shared memory mamtul kernel

--- CUDA/Matmul_Kernel/matmul.cu
+++ CUDA/Matmul_Kernel/matmul.cu
@@ -18,57 +18,55 @@ __global__ void matmul(float* A, float* B, float* C, int M, int N, int K) {
     }
 }
 
-// #define TILE_WIDTH 10
-// __global__ void matmul(int* M, int* N, int* P, int width) {
-//     __shared__ float Mds[TILE_WIDTH][TILE_WIDTH];
-//     __shared__ float Nds[TILE_WIDTH][TILE_WIDTH];
+#define TILE_WIDTH 16
+__global__ void matmul_share(float* A, float* B, float* C, int M, int N,
+                             int K) {
+    __shared__ float ds_A[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float ds_B[TILE_WIDTH][TILE_WIDTH];
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int row = by * TILE_WIDTH + ty;
+    int col = bx * TILE_WIDTH + tx;
 
-//     int bx = blockIdx.x;
-//     int by = blockIdx.y;
-//     int tx = threadIdx.x;
-//     int ty = threadIdx.y;
-
-//     int Col = bx * TILE_WIDTH + tx;
-//     int Row = by * TILE_WIDTH + ty;
-
-//     int Pervalue = 0;
-
-//     for (int i = 0; i < width / TILE_WIDTH;
-//          i++)  //有多少个TILE_WIDTH，每个循环计算一个块的大小
-//     {
-//         Mds[ty][tx] = M[Row * width + (i * TILE_WIDTH + tx)];
-//         Nds[ty][tx] = N[Col + (i * TILE_WIDTH + ty) * width];
-//         __syncthreads();
-
-//         for (int k = 0; k < TILE_WIDTH; k++)  // TILE_WIDTH相乘
-//             Pervalue += Mds[ty][k] * Nds[k][tx];
-//         __syncthreads();
-//     }
-
-//     P[Row * width + Col] = Pervalue;
-// }
-
-void init_host_matrix(float* a, float* b, int M, int N, int K) {
-    for(int i = 0; i < M; i++) {
-        for(int j = 0; j < N; j++) {
-            a[i * N + j] = 1.f;
+    float value = 0.f;
+    for (int t = 0; t < (N + TILE_WIDTH - 1) / TILE_WIDTH; t++) {
+        if (row < M && t * TILE_WIDTH + tx < N) {
+            ds_A[tx][ty] = A[row * N + t * TILE_WIDTH + tx];
+        } else {
+            ds_A[tx][ty] = 0.f;
         }
+        if (col < K && t * TILE_WIDTH + ty < N) {
+            ds_B[tx][ty] = B[(t * TILE_WIDTH + ty) * K + col];
+        } else {
+            ds_B[tx][ty] = 0.f;
+        }
+        __syncthreads();
+        for (int i = 0; i < TILE_WIDTH; i++) {
+            value += ds_A[i][ty] * ds_B[tx][i];
+        }
+        __syncthreads();
     }
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < K; j++) {
-            b[i * K + j] = 1.f;
-        }
+
+    if (row < M && col < K) {
+        C[row * K + col] = value;
     }
 }
 
+void init_host_matrix(float* a, float* b, int M, int N, int K);
+void compute_host_matrix(float* a, float* b, float* c, int M, int N, int K);
+void check_matrix(float* c, float* host_c, int M, int K);
+
 int main() {
-    const int M = 1024, N = 1024, K = 1024;
+    const int M = 10, N = 1024, K = 1024;
     int width = 16;
     float *A, *B, *C;
 
     float* a = (float*)malloc(M * N * sizeof(float));
     float* b = (float*)malloc(N * K * sizeof(float));
     float* c = (float*)malloc(M * K * sizeof(float));
+    float* host_c = (float*)malloc(M * K * sizeof(float));
 
     dim3 gridSize((M + width - 1) / width, (K + width - 1) / width);
     dim3 blockSize(width, width);
@@ -81,7 +79,7 @@ int main() {
     CUDACHECK(cudaMalloc((void**)&A, M * N * sizeof(float)));
     CUDACHECK(cudaMalloc((void**)&B, N * K * sizeof(float)));
     CUDACHECK(cudaMalloc((void**)&C, M * K * sizeof(float)));
-    
+
     init_host_matrix(a, b, M, N, K);
 
     CUDACHECK(cudaMemcpy(A, a, M * N * sizeof(float), cudaMemcpyHostToDevice));
@@ -92,15 +90,19 @@ int main() {
     after_kernel_launch();
     CUDACHECK(cudaEventRecord(stop, 0));
     CUDACHECK(cudaEventSynchronize(stop));
-    //CUDACHECK(cudaDeviceSynchronize());
+    // CUDACHECK(cudaDeviceSynchronize());
     CUDACHECK(cudaEventElapsedTime(&elapsedTime, start, stop));
 
     CUDACHECK(cudaMemcpy(c, C, M * K * sizeof(float), cudaMemcpyDeviceToHost));
-    printf("time: %fms, c0 = %f\n", elapsedTime, c[0]);
+    printf("time: %fms\n", elapsedTime);
+
+    compute_host_matrix(a, b, host_c, M, N, K);
+    check_matrix(c, host_c, M, K);
 
     free(a);
     free(b);
     free(c);
+    free(host_c);
     CUDACHECK(cudaFree(A));
     CUDACHECK(cudaFree(B));
     CUDACHECK(cudaFree(C));
@@ -108,4 +110,41 @@ int main() {
     CUDACHECK(cudaEventDestroy(stop));
 
     return 0;
+}
+
+void init_host_matrix(float* a, float* b, int M, int N, int K) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            a[i * N + j] = 1.f;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < K; j++) {
+            b[i * K + j] = 1.f;
+        }
+    }
+}
+
+void compute_host_matrix(float* a, float* b, float* c, int M, int N, int K) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < K; j++) {
+            float value = 0;
+            for (int t = 0; t < N; t++) {
+                value += a[i * N + t] * b[t * K + j];
+            }
+            c[i * K + j] = value;
+        }
+    }
+}
+
+void check_matrix(float* c, float* host_c, int M, int K) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < K; j++) {
+            if (c[i * K + j] != host_c[i * K + j]) {
+                fprintf(stderr, "check failed: c[%d][%d], host: %f, device: %f\n", i,
+                        j, host_c[i * K + j], c[i * K + j]);
+                return;
+            }
+        }
+    }
 }
